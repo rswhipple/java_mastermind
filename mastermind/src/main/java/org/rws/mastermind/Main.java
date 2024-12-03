40d3c2867cb43b709991fa7e77379982391c74d3
Rebecca W Silverstein <rswhipple@gmail.com>
2024-12-03 07:41:31 -0400

    WIP default main for testing

--- mastermind/src/main/java/org/rws/mastermind/Main.java
+++ mastermind/src/main/java/org/rws/mastermind/Main.java
@@ -1,7 +1,26 @@
 package org.rws.mastermind;
 
+import org.rws.mastermind.engine.MastermindGameEngine;
+import org.rws.mastermind.settings.DefaultSettingsProvider;
+import org.rws.mastermind.input.CLIInputHandler;
+import org.rws.mastermind.feedback.DefaultFeedbackGenerator;
+import org.rws.mastermind.codegen.DefaultCodeGenerator;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        DefaultSettingsProvider settingsProvider = new DefaultSettingsProvider();
+        CLIInputHandler inputHandler = new CLIInputHandler();
+        DefaultFeedbackGenerator feedbackGenerator = new DefaultFeedbackGenerator(settingsProvider);
+        DefaultCodeGenerator codeGenerator = new DefaultCodeGenerator(settingsProvider);
+
+        MastermindGameEngine game = new MastermindGameEngine(
+                settingsProvider, 
+                inputHandler,
+                feedbackGenerator,
+                codeGenerator
+            );
+        game.createGameSession(settingsProvider);
     }
+
 }
