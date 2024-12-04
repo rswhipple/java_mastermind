e2600650300c4b69028a1eb7822414074c02995b
Rebecca W Silverstein <rswhipple@gmail.com>
2024-12-04 09:11:48 -0400

    Fix and Refactor GameEngine logic

--- mastermind/src/main/java/org/rws/mastermind/engine/MastermindGameEngine.java
+++ mastermind/src/main/java/org/rws/mastermind/engine/MastermindGameEngine.java
@@ -7,24 +7,24 @@ import org.rws.mastermind.interfaces.FeedbackGenerator;
 import org.rws.mastermind.interfaces.CodeGenerator;
 import org.rws.mastermind.models.Code;
 import org.rws.mastermind.models.GameSession;
-import org.rws.mastermind.models.TurnResult;
+import org.rws.mastermind.models.Player;
+import org.rws.mastermind.utils.Validator;
+
 
 import java.util.List;
+import java.util.ArrayList;
 import java.util.UUID;
 
 
 public class MastermindGameEngine implements GameEngine {
-    private final InputHandler inputHandler;
-    private final FeedbackGenerator feedbackGenerator;
-    private final CodeGenerator codeGenerator;
-    private final GameSettingsProvider gameSettingsProvider;
+    private final InputHandler input;
+    private final FeedbackGenerator feedback;
+    private final CodeGenerator codeGen;
+    private final GameSettingsProvider settings;
 
-    private int getNumberOfPlayers;
-    private List<String> playerNames;
-    private int getNumberOfRounds;
-    private int getCodeLength;
-    private String sessionID;
+    private GameSession session;
     private Code secretCode;
+    private Validator validator;
     private boolean gameOver = false;
 
     public MastermindGameEngine(
@@ -33,46 +33,79 @@ public class MastermindGameEngine implements GameEngine {
             FeedbackGenerator feedbackGenerator, 
             CodeGenerator codeGenerator
         ) {
-        this.gameSettingsProvider = gameSettingsProvider;
-        this.inputHandler = inputHandler;
-        this.feedbackGenerator = feedbackGenerator;
-        this.codeGenerator = codeGenerator;
-        this.sessionID = UUID.randomUUID().toString();
-        this.getNumberOfPlayers = gameSettingsProvider.getNumberOfPlayers();
-        this.getNumberOfRounds = gameSettingsProvider.getNumberOfRounds();
-        this.getCodeLength = gameSettingsProvider.getCodeLength();
-    }
-
-    public String getSessionID() {
-        return sessionID;
-    }
-
-    public void setSessionID(String sessionID) {
-        this.sessionID = sessionID;
+        this.settings = gameSettingsProvider;
+        this.input = inputHandler;
+        this.feedback = feedbackGenerator;
+        this.codeGen = codeGenerator;
+        this.session = null;
+        this.secretCode = null;
+        this.validator = null;
     }
 
     public void resetSession() {
-        this.sessionID = UUID.randomUUID().toString();
 
     }
 
-    @Override
-    public GameSession createGameSession(GameSettingsProvider gameSettingsProvider) {
-        secretCode = codeGenerator.generateCode();
-        inputHandler.displayMessage("Welcome to Mastermind! Make your first guess.");
+    public Player createPlayer() {
+        input.displayMessage("Welcome to Mastermind! What's your name?");
+        Player player = new Player(input.getInput());
 
-        return new GameSession(this.sessionID, playerNames);
+        return player;
     }
 
-
-    @Override
-    public TurnResult processGuess(String sessionID, String guess) {
-        String feedback = feedbackGenerator.generateFeedback(secretCode, guess);
-        inputHandler.displayMessage("Feedback: " + feedback);
-
-        if (secretCode.equals(guess)) {
-            inputHandler.displayMessage("Congratulations! You've cracked the code!");
-            gameOver = true;
+    public List<Player> compilePlayersList() {
+        List<Player> players = new ArrayList<>();
+        int numPlayers = settings.getNumberOfPlayers();
+        for (int i = 0; i < numPlayers; i++) {
+            players.add(createPlayer());
         }
+
+        return players;
+    }
+
+    @Override
+    public GameSession startGameSession() {
+        // Get Player Names
+        List<Player> players = compilePlayersList();
+
+        // Generate secretCode and create Validator
+        secretCode = codeGen.generateCode();
+        validator = new Validator(settings.getCodeLength(), settings.getCodeOptions());
+    
+        // Start the game
+        String sessionID = UUID.randomUUID().toString();
+        session = new GameSession(settings, sessionID, players);
+       
+        // Game loop
+        while (!gameOver) {
+            input.displayMessage("Make a guess: ");
+            processGuess(input.getInput());
+
+            if (session.isGameOver()) {
+                gameOver = true;
+            }
+        }
+
+        return session;
+    }
+
+
+    @Override
+    public void processGuess(String guess) {
+        if (!validator.isValidGuess(guess)) {
+            input.displayMessage("Invalid guess. Please try again.");
+            return;
+        }
+
+        session.decrementAttempts();
+
+        if (secretCode.matches(guess)) {
+            input.displayMessage("Congratulations! You've cracked the code!");
+            gameOver = true;
+            return;
+        }
+
+        String result = feedback.generateFeedback(secretCode, guess);
+        input.displayMessage("Feedback: " + result);
     }
 }
