b38a9c3f731def5b5cd7e06d23e49dad1fc8d744
Rebecca W Silverstein <rswhipple@gmail.com>
2024-12-09 08:46:34 -0400

    WIP add testing for engine

---
+++ mastermind/src/test/java/org/rws/mastermind/engine/MMGameEngineTest.java
@@ -0,0 +1,32 @@
+package org.rws.mastermind.engine;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MMGameEngineTest {
+
+    @Test
+    void onMenuKey() {
+    }
+
+    @Test
+    void createGameSession() {
+    }
+
+    @Test
+    void resetSession() {
+    }
+
+    @Test
+    void endGameSession() {
+    }
+
+    @Test
+    void runGame() {
+    }
+
+    @Test
+    void processGuess() {
+    }
+}
